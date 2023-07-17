6d2f546f48a288b89ae0661e1eee7dc0c27625db
hchyi259 <hchyi259@naver.com>
2023-07-17 14:33:24 +0900

    git push -u origin master

--- back-end/src/main/java/com/ssafy/catchpalm/api/controller/HelloController.java
+++ back-end/src/main/java/com/ssafy/catchpalm/api/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.ssafy.catchpalm.Controller;
+package com.ssafy.catchpalm.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ back-end/src/main/java/com/ssafy/catchpalm/api/service/UserService.java
@@ -0,0 +1,12 @@
+package com.ssafy.catchpalm.api.service;
+
+import com.ssafy.catchpalm.api.request.UserRegisterPostReq;
+import com.ssafy.catchpalm.db.entity.User;
+
+/**
+ *	유저 관련 비즈니스 로직 처리를 위한 서비스 인터페이스 정의.
+ */
+public interface UserService {
+	User createUser(UserRegisterPostReq userRegisterInfo);
+	User getUserByUserId(String userId);
+}
