c1645dfba7766f02b88dd28911875a09e2e7776a
zzckckck3 <zzckckck3@naver.com>
2023-07-17 10:17:03 +0900

    initial commit

---
+++ catchpalm/src/main/java/com/ssafy/catchpalm/CatchpalmApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.catchpalm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchpalmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchpalmApplication.class, args);
+	}
+
+}
