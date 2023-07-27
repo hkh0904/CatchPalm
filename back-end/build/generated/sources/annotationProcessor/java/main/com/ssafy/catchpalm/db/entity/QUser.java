e4e84352ba2298ebe50f49f7c6cf5dab5315882c
hchyi259 <hchyi259@naver.com>
2023-07-27 16:03:26 +0900

    front rollback

--- back-end/build/generated/sources/annotationProcessor/java/main/com/ssafy/catchpalm/db/entity/QUser.java
+++ back-end/build/generated/sources/annotationProcessor/java/main/com/ssafy/catchpalm/db/entity/QUser.java
@@ -7,6 +7,7 @@ import com.querydsl.core.types.dsl.*;
 import com.querydsl.core.types.PathMetadata;
 import javax.annotation.Generated;
 import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
 
 
 /**
@@ -17,33 +18,66 @@ public class QUser extends EntityPathBase<User> {
 
     private static final long serialVersionUID = -439282322L;
 
+    private static final PathInits INITS = PathInits.DIRECT2;
+
     public static final QUser user = new QUser("user");
 
-    public final QBaseEntity _super = new QBaseEntity(this);
+    public final NumberPath<Integer> age = createNumber("age", Integer.class);
 
-    public final StringPath department = createString("department");
+    public final StringPath emailVerificationToken = createString("emailVerificationToken");
 
-    //inherited
-    public final NumberPath<Long> id = _super.id;
+    public final NumberPath<Integer> emailVerified = createNumber("emailVerified", Integer.class);
 
-    public final StringPath name = createString("name");
+    public final QGameRoom gameRoom;
+
+    public final DateTimePath<java.time.LocalDateTime> joinDate = createDateTime("joinDate", java.time.LocalDateTime.class);
+
+    public final ListPath<MusicLike, QMusicLike> likeList = this.<MusicLike, QMusicLike>createList("likeList", MusicLike.class, QMusicLike.class, PathInits.DIRECT2);
+
+    public final StringPath nickname = createString("nickname");
 
     public final StringPath password = createString("password");
 
-    public final StringPath position = createString("position");
+    public final NumberPath<Integer> point = createNumber("point", Integer.class);
+
+    public final SimplePath<java.sql.Blob> profileImg = createSimple("profileImg", java.sql.Blob.class);
+
+    public final SimplePath<java.sql.Blob> profileMusic = createSimple("profileMusic", java.sql.Blob.class);
+
+    public final QRank rank;
+
+    public final StringPath refreshToken = createString("refreshToken");
+
+    public final NumberPath<Integer> sex = createNumber("sex", Integer.class);
+
+    public final NumberPath<Integer> status = createNumber("status", Integer.class);
+
+    public final NumberPath<Double> synk = createNumber("synk", Double.class);
 
     public final StringPath userId = createString("userId");
 
+    public final NumberPath<Long> userNumber = createNumber("userNumber", Long.class);
+
     public QUser(String variable) {
-        super(User.class, forVariable(variable));
+        this(User.class, forVariable(variable), INITS);
     }
 
     public QUser(Path<? extends User> path) {
-        super(path.getType(), path.getMetadata());
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
     }
 
     public QUser(PathMetadata metadata) {
-        super(User.class, metadata);
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QUser(PathMetadata metadata, PathInits inits) {
+        this(User.class, metadata, inits);
+    }
+
+    public QUser(Class<? extends User> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.gameRoom = inits.isInitialized("gameRoom") ? new QGameRoom(forProperty("gameRoom"), inits.get("gameRoom")) : null;
+        this.rank = inits.isInitialized("rank") ? new QRank(forProperty("rank"), inits.get("rank")) : null;
     }
 
 }
--- back-end/src/main/java/com/ssafy/catchpalm/api/controller/OAuthController.java
+++ back-end/src/main/java/com/ssafy/catchpalm/api/controller/OAuthController.java
@@ -10,11 +10,16 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.ssafy.catchpalm.api.response.UserLoginPostRes;
+import com.ssafy.catchpalm.api.service.UserService;
+import com.ssafy.catchpalm.common.util.JwtTokenUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -42,6 +47,9 @@ public class OAuthController {
 
     private GoogleAuthorizationCodeFlow flow;
 
+    @Autowired
+    UserService userService;
+
     @GetMapping("/authorization/google")
     @ApiOperation(value = "구글 로그인 주소 반환", notes = "구글계정으로 로그인 버튼을 위한 url을 String으로 반환한다")
     public String googleLogin() {
@@ -68,7 +76,7 @@ public class OAuthController {
     }
 
     @GetMapping("/callback")
-    public String googleCallback(@RequestParam("code") String code) {
+    public ResponseEntity<UserLoginPostRes> googleCallback(@RequestParam("code") String code) {
         try {
             TokenResponse tokenResponse =
                     flow.newTokenRequest(code).setRedirectUri(CALLBACK_URI).execute();
@@ -81,12 +89,17 @@ public class OAuthController {
             String name = (String) payload.get("name");
 
             // 이제 email 및 name을 사용하여 로그인 또는 회원 가입을 처리할 수 있습니다.
+            if(userService.isDuplicatedUserId(email)){
+
+
+            }
             System.out.println(email);
             System.out.println(name);
 
-        } catch (IOException e) {
+            return ResponseEntity.ok(UserLoginPostRes.of(200, "Success", JwtTokenUtil.getToken(email)));
+
+        } catch (Exception e) {
             throw new RuntimeException("구글 로그인 오류");
         }
-        return "Success";
     }
 }
