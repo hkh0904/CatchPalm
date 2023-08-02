53c06492d5d4aa07cc633cedb4d4f3a01470e611
HKH <hkh0904@naver.com>
2023-08-02 16:40:54 +0900

    [경환] (update) 모션별 노드터트리기, 배경음악

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Grid from '@mui/material/Grid';
 import PlayingPage from './pages/PlayingPage';
+import ChatRoomItem from "./components/ChatRoomItem";
+import ChatRoomList from "./components/ChatRoomList"; // chat 리스트방으로
 import { Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
@@ -14,12 +16,21 @@ function MainPage() {
     navigate('/Playing');
   };
 
+  const handleButtonClick2 = () => {
+    navigate('/ChatRoomList');
+  };
+
   return (
     <React.Fragment>
       <Grid className="mainGrid" container spacing={2}>
         <Grid item xs={4} md={8} lg={8}>
           <Button variant="contained" onClick={handleButtonClick}>
             Go to Sample Page
+          </Button>
+          <br />
+          <br />
+          <Button variant="contained" onClick={handleButtonClick2}>
+            채팅방리스트로 가기
           </Button>
         </Grid>
       </Grid>
@@ -33,6 +44,8 @@ function App() {
       <Routes>
         <Route path="/Playing" element={<PlayingPage />} />
         <Route path="/" element={<MainPage />} />
+        <Route path="/chatRoomList" element={<ChatRoomList onSelectChatRoom={undefined} />} />
+        <Route path="/chat-rooms/:roomNumber" element={<ChatRoomItem />} />
       </Routes>
     </Router>
   );
---
+++ front-end/src/pages/SignOut.js
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+const SignOut = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // 사용자에게 확인 메시지를 보여줍니다.
+    if (!window.confirm('정말로 회원 탈퇴를 진행하시겠습니까?')) {
+      navigate('/');
+      return;
+    }
+
+    const deleteUser = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        const response = await axios.delete('http://localhost:8080/api/v1/users/delete', {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        });
+        console.log(response.data);
+
+        // 회원 탈퇴 후 로그아웃 처리를 합니다.
+        localStorage.removeItem('token');
+
+        // 메인 페이지로 이동합니다.
+        navigate('/');
+
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    deleteUser();
+  }, [navigate]);
+
+  return (
+    <div>Processing...</div>
+  );
+}
+
+export default SignOut;
