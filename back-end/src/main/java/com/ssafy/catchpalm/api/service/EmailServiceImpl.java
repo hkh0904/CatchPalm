0bcb86618ee51797c670766306e336a87489d504
zzckckck3 <65270703+zzckckck3@users.noreply.github.com>
2023-08-03 13:26:18 +0900

    Update EmailServiceImpl.java

--- back-end/src/main/java/com/ssafy/catchpalm/api/service/EmailServiceImpl.java
+++ back-end/src/main/java/com/ssafy/catchpalm/api/service/EmailServiceImpl.java
@@ -17,6 +17,9 @@ import java.nio.charset.StandardCharsets;
 @Service("emailService")
 public class EmailServiceImpl implements EmailService {
 
+    @Value("${server.address}")
+    private String serverAddress;
+    
     private final JavaMailSender emailSender;
     private static String adminEmail;
 
@@ -33,7 +36,7 @@ public class EmailServiceImpl implements EmailService {
     @Override
     public void sendVerificationEmail(String userEmail, String emailVerificationToken) throws MessagingException {
         String encodedToken = emailVerificationToken.replace("+", "%2B");
-        String verificationUrl = "https://${server.address}:8443/api/v1/auth/verifyEmail?token=" + encodedToken;
+        String verificationUrl = "https://" + serverAddress + ":8443/api/v1/auth/verifyEmail?token=" + encodedToken;
         String buttonHtml = "<a href=\"" + verificationUrl + "\" style=\"background-color: #4CAF50; border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer;\">Verify Email</a>";
         // Call your method to send an email, passing the verification URL.
         MimeMessage message = emailSender.createMimeMessage();
