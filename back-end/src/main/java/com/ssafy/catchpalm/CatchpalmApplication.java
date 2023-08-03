963104b0ed9316141735b7e806ac91b268de8763
zzckckck3 <65270703+zzckckck3@users.noreply.github.com>
2023-08-03 09:52:59 +0900

    Update CatchpalmApplication.java

--- back-end/src/main/java/com/ssafy/catchpalm/CatchpalmApplication.java
+++ back-end/src/main/java/com/ssafy/catchpalm/CatchpalmApplication.java
@@ -2,9 +2,6 @@ package com.ssafy.catchpalm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
@@ -12,11 +9,7 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 
 import java.nio.charset.StandardCharsets;
 
-@SpringBootApplication(exclude = {
-        DataSourceAutoConfiguration.class,
-        DataSourceTransactionManagerAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class
-})
+@SpringBootApplication
 public class CatchpalmApplication {
 	public static void main(String[] args) {
         SpringApplication.run(CatchpalmApplication.class, args);
