2d25ea2d0c98a7c6748537e64d176f49dd30897e
zzckckck3 <65270703+zzckckck3@users.noreply.github.com>
2023-08-03 13:09:04 +0900

    Update EmailServiceImpl.java

--- back-end/src/main/java/com/ssafy/catchpalm/api/service/EmailServiceImpl.java
+++ back-end/src/main/java/com/ssafy/catchpalm/api/service/EmailServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.io.UnsupportedEncodingException;
@@ -20,16 +21,19 @@ public class EmailServiceImpl implements EmailService {
     private static String adminEmail;
 
     @Autowired
-    public EmailServiceImpl(@Value("${spring.mail.username}") String adminEmail,JavaMailSender emailSender) {
+    public EmailServiceImpl(JavaMailSender emailSender) {
         this.emailSender = emailSender;
-        this.adminEmail = adminEmail;
     }
 
+    @PostConstruct
+    public void init() {
+        this.adminEmail = System.getenv("spring.mail.username");
+    }
 
     @Override
     public void sendVerificationEmail(String userEmail, String emailVerificationToken) throws MessagingException {
         String encodedToken = emailVerificationToken.replace("+", "%2B");
-        String verificationUrl = "https://localhost:8443/api/v1/auth/verifyEmail?token=" + encodedToken;
+        String verificationUrl = "https://${server.address}:8443/api/v1/auth/verifyEmail?token=" + encodedToken;
         String buttonHtml = "<a href=\"" + verificationUrl + "\" style=\"background-color: #4CAF50; border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer;\">Verify Email</a>";
         // Call your method to send an email, passing the verification URL.
         MimeMessage message = emailSender.createMimeMessage();
