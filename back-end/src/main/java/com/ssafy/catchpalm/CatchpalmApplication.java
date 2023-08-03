229322ec8ea34bb1a15df95a6fb1166d4dda1792
zzckckck3 <65270703+zzckckck3@users.noreply.github.com>
2023-08-03 09:48:48 +0900

    Update CatchpalmApplication.java

--- back-end/src/main/java/com/ssafy/catchpalm/CatchpalmApplication.java
+++ back-end/src/main/java/com/ssafy/catchpalm/CatchpalmApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 
 import java.nio.charset.StandardCharsets;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class CatchpalmApplication {
 	public static void main(String[] args) {
         SpringApplication.run(CatchpalmApplication.class, args);
