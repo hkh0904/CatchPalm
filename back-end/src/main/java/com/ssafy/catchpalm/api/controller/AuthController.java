0c555b8f4379412ee55a0e144dae4c839e2a71d0
HKH <hkh0904@naver.com>
2023-08-03 10:23:04 +0900

    123

--- back-end/src/main/java/com/ssafy/catchpalm/api/controller/AuthController.java
+++ back-end/src/main/java/com/ssafy/catchpalm/api/controller/AuthController.java
@@ -41,7 +41,6 @@ public class  AuthController {
 
 	@Autowired
 	PasswordEncoder passwordEncoder;
-<<<<<<< HEAD
 
 	@PostMapping(value="/login")
 	@ApiOperation(value = "로그인", notes = "<strong>아이디와 패스워드</strong>를 통해 로그인 한다.")
@@ -50,16 +49,6 @@ public class  AuthController {
 			@ApiResponse(code = 401, message = "인증 실패", response = BaseResponseBody.class),
 			@ApiResponse(code = 500, message = "사용자 없음", response = BaseResponseBody.class)
 	})
-=======
-	
-	@PostMapping(value="/login")
-	@ApiOperation(value = "로그인", notes = "<strong>아이디와 패스워드</strong>를 통해 로그인 한다.") 
-    @ApiResponses({
-        @ApiResponse(code = 200, message = "성공", response = UserLoginPostRes.class),
-        @ApiResponse(code = 401, message = "인증 실패", response = BaseResponseBody.class),
-        @ApiResponse(code = 500, message = "사용자 없음", response = BaseResponseBody.class)
-    })
->>>>>>> 16904f376a12670b2941267e2bece7235ce2c64c
 	public ResponseEntity<UserLoginPostRes> login(@RequestBody @ApiParam(value="로그인 정보", required = true) UserLoginPostReq loginInfo) throws Exception {
 		String userId = "local:"+loginInfo.getUserId();
 		String password = loginInfo.getPassword();
