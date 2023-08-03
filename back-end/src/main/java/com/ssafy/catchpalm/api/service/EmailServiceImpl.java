72f0981a627fb8097ddb576db54c6a0205669348
zzckckck3 <65270703+zzckckck3@users.noreply.github.com>
2023-08-03 10:38:58 +0900

    (Leej) Setting build.gradle, Dockerfile, application.properties

--- back-end/src/main/java/com/ssafy/catchpalm/api/service/EmailServiceImpl.java
+++ back-end/src/main/java/com/ssafy/catchpalm/api/service/EmailServiceImpl.java
@@ -29,7 +29,7 @@ public class EmailServiceImpl implements EmailService {
     @Override
     public void sendVerificationEmail(String userEmail, String emailVerificationToken) throws MessagingException {
         String encodedToken = emailVerificationToken.replace("+", "%2B");
-        String verificationUrl = "http://localhost:8080/api/v1/auth/verifyEmail?token=" + encodedToken;
+        String verificationUrl = "http://${server.address}:8080/api/v1/auth/verifyEmail?token=" + encodedToken;
         String buttonHtml = "<a href=\"" + verificationUrl + "\" style=\"background-color: #4CAF50; border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer;\">Verify Email</a>";
         // Call your method to send an email, passing the verification URL.
         MimeMessage message = emailSender.createMimeMessage();
