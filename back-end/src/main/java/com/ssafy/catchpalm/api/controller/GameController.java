fc58151128227de1c4cafe13baede155a1bc84bc
hchyi259 <hchyi259@naver.com>
2023-08-04 17:31:19 +0900

    update

--- back-end/src/main/java/com/ssafy/catchpalm/api/controller/GameController.java
+++ back-end/src/main/java/com/ssafy/catchpalm/api/controller/GameController.java
@@ -1,27 +1,52 @@
 package com.ssafy.catchpalm.api.controller;
 
-import com.ssafy.catchpalm.api.service.LocalStorageServiceImpl;
-import com.ssafy.catchpalm.api.service.StorageService;
+import com.ssafy.catchpalm.api.request.*;
+import com.ssafy.catchpalm.api.response.GameRoomPostRes;
+import com.ssafy.catchpalm.api.response.UserRes;
+import com.ssafy.catchpalm.api.service.GameRoomService;
+import com.ssafy.catchpalm.api.service.UserService;
+import com.ssafy.catchpalm.common.auth.SsafyUserDetails;
+import com.ssafy.catchpalm.common.model.response.BaseResponseBody;
+import com.ssafy.catchpalm.db.entity.GameRoom;
+import com.ssafy.catchpalm.db.entity.GameRoomUserInfo;
+import com.ssafy.catchpalm.db.entity.User;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 게임 관련 API 요청 처리를 위한 컨트롤러 정의.
+ */
+@Api(value = "게임 API", tags = {"Game"})
 @RestController
+@RequestMapping("/api/v1/game")
 public class GameController {
 
     @Autowired
-    private final StorageService storageService;
+    GameRoomService gameRoomService;
 
-    public GameController(LocalStorageServiceImpl storageService) {
-        this.storageService = storageService;
-    }
+    @PostMapping("/log")
+    @ApiOperation(value = "로그 기록", notes = "<strong>게임 기록을 넘겨주면</strong>로그를 기록한다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 401, message = "인증 실패"),
+            @ApiResponse(code = 404, message = "사용자 없음"),
+            @ApiResponse(code = 500, message = "서버 오류")
+    })
+    public ResponseEntity<BaseResponseBody> createLog(
+            @RequestBody @ApiParam(value="방 정보", required = true) GameLogPostReq gameInfo) {
 
-    @GetMapping("/files/{filename:.+}")
-    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
-        Resource file = storageService.load(filename);
-        return ResponseEntity.ok().body(file);
+        // 로그 기록
+        GameRoom gameRoom = gameRoomService.createRoom(gameInfo);
+        // 랭킹 업데이트
+
+        return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
     }
 }
---
+++ back-end/src/main/java/com/ssafy/catchpalm/api/service/GameServiceImpl.java
@@ -0,0 +1,32 @@
+package com.ssafy.catchpalm.api.service;
+
+import com.ssafy.catchpalm.api.request.GameLogPostReq;
+import com.ssafy.catchpalm.db.entity.Music;
+import com.ssafy.catchpalm.db.entity.Records;
+import com.ssafy.catchpalm.db.entity.User;
+import com.ssafy.catchpalm.db.repository.RankRepository;
+import com.ssafy.catchpalm.db.repository.RecordsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class GameServiceImpl implements GameService {
+
+    @Autowired
+    RankRepository rankRepository;
+
+    @Autowired
+    RecordsRepository recordsRepository;
+
+    @Override
+    public void createLog(GameLogPostReq gameInfo){
+        Records records = new Records();
+        User user = new User();
+        Music music = new Music();
+        music.setMusicNumber(gameInfo.getMusicNumber());
+        records.setMusic(music);
+        user.setUserNumber(gameInfo.getUserNumber());
+        records.setUser(user);
+        records.setScore(gameInfo.getScore());
+
+    }
+
+}
