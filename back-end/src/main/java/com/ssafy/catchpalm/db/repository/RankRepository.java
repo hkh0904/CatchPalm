51416f44a4f60be3a17c7355e11fc25c867a2d0e
hchyi259 <hchyi259@naver.com>
2023-08-07 01:22:08 +0900

    이수민 (add) 랭킹, 로그 시스템 추가

--- back-end/src/main/java/com/ssafy/catchpalm/db/repository/RankRepository.java
+++ back-end/src/main/java/com/ssafy/catchpalm/db/repository/RankRepository.java
@@ -5,7 +5,13 @@ import com.ssafy.catchpalm.db.entity.Rank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface RankRepository extends JpaRepository<Rank, Integer> {
+
+    Optional<Rank> findByRankUserUserNumberAndMusicMusicNumber(long userNumber, int musicNumber);
+    List<Rank> findByMusicMusicNumber(int musicNumber);
 
 }
