728a7a0bb345cd0ea53d0fe438eb08871ee6e5f9
이재진 <zzckckck3@naver.com>
2023-08-16 15:32:10 +0900

    Update back-end/src/main/java/com/ssafy/catchpalm/api/service/EmailServiceImpl.java

--- back-end/src/main/java/com/ssafy/catchpalm/api/service/EmailServiceImpl.java
+++ back-end/src/main/java/com/ssafy/catchpalm/api/service/EmailServiceImpl.java
@@ -17,7 +17,7 @@ import java.nio.charset.StandardCharsets;
 @Service("EmailService")
 public class EmailServiceImpl implements EmailService {
 
-    @Value("${server.address}")
+    @Value("${server.address.port}")
     String serverAddress;
     private final JavaMailSender emailSender;
     @Value("${spring.mail.username}")
@@ -31,8 +31,8 @@ public class EmailServiceImpl implements EmailService {
     @Override
     public void sendVerificationEmail(String userEmail, String emailVerificationToken) throws MessagingException {
         String encodedToken = emailVerificationToken.replace("+", "%2B");
-        String verificationUrl = "https://localhost:8443/api/v1/auth/verifyEmail?token=" + encodedToken;
-        //String verificationUrl = "https://"+serverAddress+"api/api/v1/auth/verifyEmail?token=" + encodedToken;
+        //String verificationUrl = "https://localhost:8443/api/v1/auth/verifyEmail?token=" + encodedToken;
+        String verificationUrl = "https://"+serverAddress+"/api/v1/auth/verifyEmail?token=" + encodedToken;
         String buttonHtml = "<a href=\"" + verificationUrl + "\" style=\"background-color: #4CAF50; border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer;\">Verify Email</a>";
         // Call your method to send an email, passing the verification URL.
         MimeMessage message = emailSender.createMimeMessage();
