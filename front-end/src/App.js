ab11d5402f37bc60533c5f9a5475afa48430f049
홍경환 <hkh0904@naver.com>
2023-08-17 13:33:12 +0900

    Update front-end/src/App.js, front-end/src/pages/ResultPage.js, front-end/src/components/ChatRoomComponents/ChatRoomList.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -27,6 +27,32 @@ function MainPage() {
   const searchParams = new URLSearchParams(location.search);
   const tokenValue = searchParams.get('token');
 
+useEffect(() => {
+    // 뒤로 가기 막기
+    const blockBack = () => {
+      window.history.pushState(null, "", window.location.href);
+    };
+
+    // 초기 실행 시 히스토리 항목 추가
+    blockBack();
+
+    window.addEventListener("popstate", blockBack);
+
+    // 새로고침 막기 (F5와 Ctrl+R)
+    const blockRefresh = (event) => {
+      if (event.keyCode === 116 || (event.ctrlKey && event.keyCode === 82)) {
+        event.preventDefault();
+      }
+    };
+
+    document.addEventListener("keydown", blockRefresh);
+
+    return () => {
+      window.removeEventListener("popstate", blockBack);
+      document.removeEventListener("keydown", blockRefresh);
+    };
+  }, []);
+
   if(tokenValue){
     localStorage.setItem('token', searchParams.get('token'));
     navigate("/");
--- front-end/src/components/ChatRoomComponents/ChatRoomList.js
+++ front-end/src/components/ChatRoomComponents/ChatRoomList.js
@@ -72,6 +72,31 @@ const Modal = ({ isOpen, onClose, onCreateRoom }) => {
       });
   }, [token]);
 
+useEffect(() => {
+    // 뒤로 가기 막기
+    const blockBack = () => {
+      window.history.pushState(null, "", window.location.href);
+    };
+
+    // 초기 실행 시 히스토리 항목 추가
+    blockBack();
+
+    window.addEventListener("popstate", blockBack);
+
+    // 새로고침 막기 (F5와 Ctrl+R)
+    const blockRefresh = (event) => {
+      if (event.keyCode === 116 || (event.ctrlKey && event.keyCode === 82)) {
+        event.preventDefault();
+      }
+    };
+
+    document.addEventListener("keydown", blockRefresh);
+
+    return () => {
+      window.removeEventListener("popstate", blockBack);
+      document.removeEventListener("keydown", blockRefresh);
+    };
+  }, []);
   
   const [roomData, setRoomData] = useState({
     capacity: '',
